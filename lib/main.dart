3eeb53cbae7169f5fa506fdf0941480b12d787f1
Vladyslav <vlmoon.near@gmail.com>
2024-12-12 14:25:32 +0200

    (init) : init supabase

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,19 @@ import 'package:near_social_mobile/assets/localizations/localizations_strings.da
 import 'package:near_social_mobile/config/theme.dart';
 import 'package:near_social_mobile/exceptions/exceptions.dart';
 import 'package:near_social_mobile/modules/app_module.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'config/setup.dart';
 
 void main() async {
+  //Add here your key's
+  await Supabase.initialize(
+    url: 'http://127.0.0.1:54323',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
+  );
+
+  WidgetsFlutterBinding.ensureInitialized();
   final app = EasyLocalization(
     supportedLocales: const [
       Locale('en'),
